20e5e39316bc3d2d86a3fc31012b3c4948775eca
Guilhem Martin <226993+guilhem-martin@users.noreply.github.com>
2021-01-10 17:49:19 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 for (let index = 0; index < 4; index++) {
-    pins.servoWritePin(AnalogPin.P0, 180)
+    basic.clearScreen()
+    pins.servoWritePin(AnalogPin.P1, 180)
     basic.pause(500)
-    pins.servoWritePin(AnalogPin.P0, 0)
+    pins.servoWritePin(AnalogPin.P1, 0)
     basic.pause(500)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 }
